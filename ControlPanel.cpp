2247b6058b0985b76f8fd6de379c951d2f5e7107
Flikersit <104367218+Flikersit@users.noreply.github.com>
2024-12-03 15:13:29 +0100

    Update ControlPanel.cpp

--- ControlPanel.cpp
+++ ControlPanel.cpp
@@ -8,21 +8,72 @@ ControlPanel::ControlPanel(QWidget *parent = nullptr, RocketObject *rocket, Rock
     this.scene = scene;
 
     resetButton = new QPushButton("Reset");
-    statusLabel = new QLabel("Tattatatta");
+    leftButton = new QPushButton("Left");
+    rightButton = new QPushButton("Right");
+
+
+    statusLabel = new QLabel("Rocket state");
+    connection = new QLabel("Disconnected");
+
+    thrustSlider = new QSlider();
+
+    networkManager = new QNetworkAccessManager(this);
     
 
-    connect(resetButton, &QPushButton::released, this, &ControlPanel::resetSimulation);
+    connect(&resetButton, &QPushButton::released, this, &ControlPanel::resetSimulation);
+    connect(&timer, &QTimer::timeout, this, &MyWidget::requestData);
+
+
+    timer.start(250);
+}
+
+
+void ControlPanel::requestData(){
+    QUrl url = QUrl("http://localhost:8008/api/data/pidcontrol_control/PIDU:pv?data&mime=application/json");
+
+	QNetworkRequest request = QNetworkRequest(url);
+
+	// HTTP Basic authentication header value: base64(username:password)
+	QString username = "admin";
+	QString password = "";
+	QString concatenated = username + ":" + password;
+	QByteArray data = concatenated.toLocal8Bit().toBase64();
+	QString headerData = "Basic " + data;
+	request.setRawHeader("Authorization", headerData.toLocal8Bit());
+	request.setRawHeader("Content-Type", "application/json");
+
+	QSslConfiguration conf = request.sslConfiguration();
+	conf.setPeerVerifyMode(QSslSocket::VerifyNone);
+	request.setSslConfiguration(conf);
+
+	QNetworkReply* reply = networkManager->get(request);
+
+	connect(reply, &QNetworkReply::errorOccurred, this, &MyWidget::onError);
+	connect(reply, &QNetworkReply::readyRead, this, &MyWidget::onDataReceived);
 }
 
 
 
+void ControlPanel::onDataReceived(){
 
-ControlPanel::resetSimulation(){
-    scene->reset;
+    connection->setText("Connected"); 
+
 }
 
 
-ControlPanel::keyPressEvent(QKeyEvent *event){
+void ControlPanel::onError(){
+
+    connection->setText("Disconnected");
+
+}
+
+
+void ControlPanel::resetSimulation(){
+    scene->reset();
+}
+
+
+void ControlPanel::keyPressEvent(QKeyEvent *event){
 
     if (event->key()==Qt::Key_R){
 		resetSimulation();
