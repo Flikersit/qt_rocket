3deef207a1754b64b05f567dc87248603b8c0516
Flikersit <104367218+Flikersit@users.noreply.github.com>
2024-12-04 12:35:38 +0100

    Update RocketScene.cpp

--- RocketScene.cpp
+++ RocketScene.cpp
@@ -2,23 +2,24 @@
 #include <QGraphicsRectItem>
 
 RocketScene::RocketScene(){
-    height = 1000;
-    width = 1200;
+    height = 600;
+    width = 800;
     launchPadOffSet = 50;
 
     setBackgroundBrush(Qt::black);
-    setSceneRect(0, 0, width, height);
+    setSceneRect(-width/2, height, width, height);
 }
 
 void RocketScene::paint_platforms(){
+
     QGraphicsRectItem *rect_1 = new QGraphicsRectItem(0, 0, 5, 10);
     QGraphicsRectItem *rect_2 = new QGraphicsRectItem(0, 0, 5, 10);
 
     rect_1->setBrush(QColor(165, 42, 42));
     rect_2->setBrush(QColor(165, 42, 42));
 
-    rect_1->setPos(launchPadOffSet, 0);
-    rect_2->setPos(width-launchPadOffSet, 0);
+    rect_1->setPos(launchPadOffSet - width/2, 0);
+    rect_2->setPos(width/2 - launchPadOffSet, 0);
 
     QGraphicsRectItem *rect_3 = new QGraphicsRectItem(0, 0, 7, 4);
     QGraphicsRectItem *rect_4 = new QGraphicsRectItem(0, 0, 7, 4);
@@ -26,13 +27,14 @@ void RocketScene::paint_platforms(){
     rect_3->setBrush(Qt::gray);
     rect_4->setBrush(Qt::gray);
 
-    rect_3->setPos(width-launchPadOffSet, 10);
-    rect_4->setPos(width-launchPadOffSet, 10);
+    rect_3->setPos(launchPadOffSet - width/2, 10);
+    rect_4->setPos(width/2 - launchPadOffSet, 10);
 
     addItem(rect_1);
     addItem(rect_2);
     addItem(rect_3);
     addItem(rect_4);
+    
 }
 
 void RocketScene::reset(){
