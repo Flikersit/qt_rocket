7172c12415b622c90dbce76c44ffc2985631e799
Flikersit <104367218+Flikersit@users.noreply.github.com>
2024-12-09 14:39:25 +0100

    Update ControlPanel.cpp

--- ControlPanel.cpp
+++ ControlPanel.cpp
@@ -11,11 +11,13 @@
 ControlPanel::ControlPanel(QWidget *parent, RocketScene *scene){
 
     this->scene = scene;
+    reset = false;
 
+    
     resetButton = new QPushButton("Reset");
-    leftButton = new QPushButton("Left");
-    rightButton = new QPushButton("Right");
+    
 
+    //switch left/right
     QWidget *check_widget = new QWidget();
     QVBoxLayout *check_box_layout = new QVBoxLayout(this);
     check_label = new QLabel("Switch enginees");
@@ -32,23 +34,48 @@ ControlPanel::ControlPanel(QWidget *parent, RocketScene *scene){
     check_box_layout->addWidget(checkbox_right);
     check_widget->setLayout(check_box_layout);
 
-
-
+    //status
+    QWidget *status_widget = new QWidget();
+    QVBoxLayout *statusbox = new QVBoxLayout(this);
     statusLabel = new QLabel("Rocket state: ");
     connection = new QLabel("Connection state: " + QVariant(scene->rocket->isconnected).toString() );
+    connection->setStyleSheet("color: black; background-color: #f5f5f5; padding: 5px;");
+    statusLabel->setStyleSheet("color: black; background-color: #f5f5f5; padding: 5px;");
+    statusbox->addWidget(connection);
+    statusbox->addWidget(statusLabel);
+    status_widget->setLayout(statusbox);
 
-    thrustSlider = new QSlider();
 
+    //Slider for the main enginee
+    QWidget *widget_for_slider = new QWidget();            
+    QVBoxLayout *sliderbox = new QVBoxLayout(this);
+    QLabel *label_for_slider = new QLabel("Thrust Slider");
+    label_for_slider->setAlignment(Qt::AlignCenter);
+    label_for_slider->setStyleSheet("color: black; background-color: #f5f5f5; padding: 5px;");
+    thrustSlider = new QSlider(Qt::Horizontal, this);
+    thrustSlider->setMinimum(0);
+    thrustSlider->setMaximum(20);
+    thrustSlider->setValue(0);
+    thrustSlider->setSingleStep(1);
+    sliderbox->addWidget(label_for_slider);
+    sliderbox->addWidget(thrustSlider);
+    widget_for_slider->setLayout(sliderbox);
+
+
+    //position
     scene->rocket->refresh_position();
     QWidget *coordinate_widget = new QWidget(); 
     QVBoxLayout *coordinate_layout = new QVBoxLayout(this);
     xposition = new QLabel("X position: " +  QString::number(scene->rocket->x));
     yposition = new QLabel("Y position: " +  QString::number(scene->rocket->y));
-    rortation = new QLabel("Rotation: ");
+    rotation = new QLabel("Rotation: 0");
+    xposition->setStyleSheet("color: black; background-color: #f5f5f5; padding: 5px;");
+    yposition->setStyleSheet("color: black; background-color: #f5f5f5; padding: 5px;");
+    rotation->setStyleSheet("color: black; background-color: #f5f5f5; padding: 5px;");
 
     coordinate_layout->addWidget(xposition);
     coordinate_layout->addWidget(yposition);
-    coordinate_layout->addWidget(rortation);
+    coordinate_layout->addWidget(rotation);
     coordinate_widget->setLayout(coordinate_layout);
 
     QHBoxLayout *layout = new QHBoxLayout(this);
@@ -80,14 +107,11 @@ ControlPanel::ControlPanel(QWidget *parent, RocketScene *scene){
 
 
     layout->addWidget(resetButton);
-    layout->addWidget(leftButton);
-    layout->addWidget(rightButton);
-    layout->addWidget(statusLabel);
-    layout->addWidget(connection);
+    layout->addWidget(status_widget);
     layout->addWidget(coordinate_widget);
-    layout->addWidget(chartView);
     layout->addWidget(check_widget);
-    layout->addWidget(thrustSlider);
+    layout->addWidget(widget_for_slider);
+    layout->addWidget(chartView);
 
     QWidget *hwidget = new QWidget();
     hwidget->setLayout(layout);
@@ -98,6 +122,8 @@ ControlPanel::ControlPanel(QWidget *parent, RocketScene *scene){
     QTransform transform;//IT WORKS DONT TOUCH
     transform.scale(1, -1);//!!
     view->setTransform(transform);//!!
+    view->setRenderHint(QPainter::Antialiasing);
+    view->fitInView(scene->sceneRect(), Qt::KeepAspectRatio);
 
     vlayout->addWidget(view);
 
@@ -111,6 +137,7 @@ ControlPanel::ControlPanel(QWidget *parent, RocketScene *scene){
 
     connect(checkbox_left, &QCheckBox::stateChanged, this, &ControlPanel::left_engine_change);
     connect(checkbox_right, &QCheckBox::stateChanged, this, &ControlPanel::right_engine_change);
+    connect(thrustSlider, &QSlider::valueChanged, this, &ControlPanel::post_data);
 
 
     timer.start(250);
@@ -156,12 +183,48 @@ void ControlPanel::onDataReceived(){
     double width = jResponse["subitems"].toArray()[4].toObject()["Width"].toObject()["v"].toDouble();
     double rotation = jResponse["subitems"].toArray()[10].toObject()["Rotation"].toObject()["v"].toDouble();
 
-    bool right_engine = jResponse["subitems"].toArray()[1].toObject()["LeftThruster"].toObject()["v"].toBool();
+    bool left_engine = jResponse["subitems"].toArray()[1].toObject()["LeftThruster"].toObject()["v"].toBool();
     bool right_engine = jResponse["subitems"].toArray()[2].toObject()["RightThruster"].toObject()["v"].toBool();
 
+    double x_position = jResponse["subitems"].toArray()[6].toObject()["X"].toObject()["v"].toDouble();
+    double y_position = jResponse["subitems"].toArray()[7].toObject()["Y"].toObject()["v"].toDouble();
+
+    double lounch_pad = jResponse["subitems"].toArray()[13].toObject()["LaunchpadOffset"].toObject()["v"].toDouble();
+
+    double vx = jResponse["subitems"].toArray()[8].toObject()["VX"].toObject()["v"].toDouble();
+    double vy = jResponse["subitems"].toArray()[8].toObject()["VY"].toObject()["v"].toDouble();
+
+    bool touchdown = jResponse["subitems"].toArray()[11].toObject()["Touchdown"].toObject()["v"].toBool();
+    bool crashed = jResponse["subitems"].toArray()[12].toObject()["Crashed"].toObject()["v"].toBool();
+
+    bool reset = jResponse["subitems"].toArray()[3].toObject()["Reset"].toObject()["v"].toBool();
+
+    
+    vxSet->replace(0, vx);
+    vySet->replace(0, vy);
 
     scene->rocket->isconnected = true;
-    connection->setText("Connection state: " + QVariant(scene->rocket->isconnected).toString() ); 
+    scene->rocket->x = x_position;
+    scene->rocket->y = y_position;
+    scene->rocket->rotation = rotation;
+
+    xposition->setText("X position: " + QString::number(scene->rocket->x));
+    yposition->setText("Y position: " + QString::number(scene->rocket->y));
+    this->rotation->setText("Rotation: " + QString::number(scene->rocket->rotation));
+
+    if(touchdown){
+        statusLabel->setText("Rocket state: Touchdown");
+    }else if(crashed){
+        statusLabel->setText("Rocket state: Crashed");
+    }else{
+        statusLabel->setText("Rocket state: In air");
+    }
+    connection->setText("Connection state: " + QVariant(scene->rocket->isconnected).toString() );
+
+    scene->width = width/2;
+    scene->height = height;
+    scene->launchPadOffSet = lounch_pad;
+    scene->paint_position(x_position, y_position);
 
 }
 
@@ -187,7 +250,7 @@ void ControlPanel::post_data(){
     QJsonArray subitems;
 
     QJsonObject engineThrottle;
-    engineThrottle["EngineThrottle"] = QJsonObject{{"v", 0.0}};
+    engineThrottle["EngineThrottle"] = QJsonObject{{"v", double(thrustSlider->value()/10)}};
     subitems.append(engineThrottle);
 
     QJsonObject leftThruster;
@@ -199,15 +262,15 @@ void ControlPanel::post_data(){
     subitems.append(rightThruster);
 
     QJsonObject reset;
-    reset["Reset"] = QJsonObject{{"v", false}};
+    reset["BSTATE"] = QJsonObject{{"v", this->reset}};
     subitems.append(reset);
 
     QJsonObject width;
-    width["Width"] = QJsonObject{{"v", 800.0}};
+    width["Width"] = QJsonObject{{"v", scene->width}};
     subitems.append(width);
 
     QJsonObject height;
-    height["height"] = QJsonObject{{"v", 600.0}};
+    height["Height"] = QJsonObject{{"v", scene->height}};
 
     QJsonObject rootObject;
     rootObject["subitems"] = subitems;
@@ -245,6 +308,12 @@ void ControlPanel::keyPressEvent(QKeyEvent *event){
         checkbox_left->setChecked(!(checkbox_left->isChecked()));
     } else if (event->key()==Qt::Key_D){
         checkbox_right->setChecked(!(checkbox_right->isChecked()));
+    } else if (event->key()==Qt::Key_W){
+        int value = thrustSlider->value();
+        thrustSlider->setValue(value+1);
+    } else if (event->key()==Qt::Key_S){
+        int value = thrustSlider->value();
+        thrustSlider->setValue(value-1);
     }
 
 }
@@ -252,11 +321,23 @@ void ControlPanel::keyPressEvent(QKeyEvent *event){
 void ControlPanel::left_engine_change(){
 
     scene->rocket->leftEngine = checkbox_left->isChecked();
+    this->post_data();
 
 }
 
 void ControlPanel::right_engine_change(){
 
     scene->rocket->rightEngine = checkbox_right->isChecked();
+    this->post_data();
 
+}
+
+void ControlPanel::resizeEvent(QResizeEvent *event) {
+    QWidget::resizeEvent(event);
+    if (scene) {
+        QGraphicsView *view = findChild<QGraphicsView *>();
+        if (view) {
+            view->fitInView(scene->sceneRect(), Qt::KeepAspectRatio);
+        }
+    }
 }
