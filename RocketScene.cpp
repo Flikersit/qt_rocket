ef44207611f5fda0996dc5bbadb62a07dd84521e
Flikersit <104367218+Flikersit@users.noreply.github.com>
2024-12-16 17:17:56 +0100

    Update RocketScene.cpp

--- RocketScene.cpp
+++ RocketScene.cpp
@@ -1,57 +1,73 @@
 #include "RocketScene.h"
 #include <QGraphicsRectItem>
 #include <QTransform>
+#include <QDebug>
 
 RocketScene::RocketScene(RocketObject *rocket){
     this->rocket = rocket;
-    height = 400.0;
-    width = 300.0;
-    launchPadOffSet = 50.0;
+
+    height = 600.0;
+    width = 1000.0;
+    launchPadOffSet = 100.0;
+    aspectRatio = static_cast<double>(width)/height;
+
+    rect_3 = new QGraphicsRectItem(0, 0, 50, 10);
+    rect_4 = new QGraphicsRectItem(0, 0, 50, 10);
+    rect_5 = new QGraphicsRectItem(0, 0, 40, 40);
+
+    QPolygonF arrow;
+    arrow << QPointF(0, 20)
+          << QPointF(-10, 0)
+          << QPointF(-5, 0)
+          << QPointF(-5, -20)
+          << QPointF(5, -20)
+          << QPointF(5, 0)
+          << QPointF(10, 0);
+
+    arrowItem = new QGraphicsPolygonItem(arrow);
+
 
     setBackgroundBrush(Qt::black);
     setSceneRect(-(width/2), 0, width, height);//it works dont touch
+    
+    paint_begin();
+
+    
+
 
 }
 
-void RocketScene::paint_position(double x, double y){
-    
+void RocketScene::paint_begin(){
     setSceneRect(-(width/2), 0, width, height);
 
-    QGraphicsRectItem *rect_1 = new QGraphicsRectItem(0, 0, 20, 40);
-    QGraphicsRectItem *rect_2 = new QGraphicsRectItem(0, 0, 20, 40);
-
-    rect_1->setBrush(QColor(165, 42, 42));
-    rect_2->setBrush(QColor(165, 42, 42));
-    rect_1->setBrush(Qt::white);
-    rect_2->setBrush(Qt::white);
-
-    rect_1->setPos(launchPadOffSet - width, 0);
-    rect_2->setPos(width - launchPadOffSet, 0);
-
-    QGraphicsRectItem *rect_3 = new QGraphicsRectItem(0, 0, 50, 10);
-    QGraphicsRectItem *rect_4 = new QGraphicsRectItem(0, 0, 50, 10);
 
     rect_3->setBrush(Qt::blue);
     rect_4->setBrush(Qt::blue);
 
-    rect_3->setPos(launchPadOffSet - width -15, 40);
-    rect_4->setPos(width - launchPadOffSet - 15, 40);
+    rect_3->setPos(launchPadOffSet - width/2, 10);
+    rect_4->setPos(width/2 - launchPadOffSet -50, 10);
 
-    rocket->setPos(x, y + 85.0);
+    //rect_5->setBrush(Qt::white);
+    //rect_5->setPos(-20, -20);
+    arrowItem->setBrush(Qt::white);
+    arrowItem->setPos(-10, 20);
 
+    rocket->setPos(launchPadOffSet - width/2 + 25 , 46);
 
-    addItem(rect_1);
-    addItem(rect_2);
+    addItem(arrowItem);
     addItem(rect_3);
     addItem(rect_4);
+    //addItem(rect_5);
     addItem(rocket);
 
     qDebug() << "Item's in scene" << items().size();
 
 }
 
-void RocketScene::reset(){
-    clear();
-    paint_position(0.0, 0.0);
-    rocket->refresh_position();
+
+void RocketScene::paint_position(double x, double y){
+    qDebug() << "Rocket disappear(";
+    rocket->setPos(x-43, y+46);
+    rocket->setRotation(round(((rocket->rotation)*(180.0/M_PI))*100)/100);
+    this->update();
 }
