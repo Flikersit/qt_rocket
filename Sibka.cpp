1de86b87db52d9ccbe8000672cf46bf3e97f4ba6
Flikersit <104367218+Flikersit@users.noreply.github.com>
2024-12-17 11:44:27 +0100

    Update Sibka.cpp

--- Sibka.cpp
+++ Sibka.cpp
@@ -4,6 +4,19 @@
 #include <QDebug>
 
 Sibka::Sibka(){
-    
+    QPolygonF arrow;
+    arrow << QPointF(0, 20)
+          << QPointF(-10, 0)
+          << QPointF(-5, 0)
+          << QPointF(-5, -20)
+          << QPointF(5, -20)
+          << QPointF(5, 0)
+          << QPointF(10, 0);
+        
+    arrowItem = new QGraphicsPolygonItem(arrow);
+
+}
+
+void Sibka::begin_draw(){
 
 }
