cf74e1989ba5ccb1bc97eb4a2d39d61d38e28bea
Flikersit <104367218+Flikersit@users.noreply.github.com>
2025-04-08 15:10:53 +0200

    Update Sibka.cpp

--- Sibka.cpp
+++ Sibka.cpp
@@ -8,7 +8,7 @@ Sibka::Sibka(QWidget *parent) : QWidget(parent) {
     // 
     scene = new QGraphicsScene(this);
     
-    scene->setSceneRect(-150, -150, 300, 300); // 
+    scene->setSceneRect(-50, -50, 100, 100); // 
 
     arrowLine = new QGraphicsLineItem(0, 0, 0, -50);
     arrowLine->setPen(QPen(Qt::red, 3));
@@ -17,17 +17,17 @@ Sibka::Sibka(QWidget *parent) : QWidget(parent) {
     // 
     view = new QGraphicsView(scene, this);
     view->setRenderHint(QPainter::Antialiasing);
-    view->setFixedSize(300, 300); // 
+    view->setFixedSize(100, 100); // 
     view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view->setBackgroundBrush(Qt::black);
 
-    qDebug() << "Scene and arrow created, positioned at center.";
+    //qDebug() << "Scene and arrow created, positioned at center.";
 }
 
 void Sibka::update_arrow(qreal angle, qreal speed) {
     // 
-    qreal length = std::min(150.0, speed); // 
+    qreal length = std::min(100.0, speed); // 
     qreal radian = qDegreesToRadians(angle);
 
     // 
@@ -36,5 +36,5 @@ void Sibka::update_arrow(qreal angle, qreal speed) {
 
     arrowLine->setLine(0, 0, -x, y);
 
-    qDebug() << "Arrow updated: angle =" << angle << "degrees, speed =" << speed;
+    //qDebug() << "Arrow updated: angle =" << angle << "degrees, speed =" << speed;
 }
