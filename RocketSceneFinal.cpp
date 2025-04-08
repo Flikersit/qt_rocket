10df6227be022c8a3d3eab71e86def91bef6e979
Flikersit <104367218+Flikersit@users.noreply.github.com>
2025-04-08 15:03:37 +0200

    Update RocketSceneFinal.cpp

--- RocketSceneFinal.cpp
+++ RocketSceneFinal.cpp
@@ -1,29 +1,62 @@
 #include <QPainter>
 #include <QSizePolicy>
 #include "RocketSceneFinal.h"
-#include <QtGlobal>
 
 RocketSceneFinal::RocketSceneFinal(QWidget *parent){
     aspectratio = 0.6;
     launchPadOffSet = 30;
     rotation = 0.0;
+    defaultWidth = 400;
+    defaultHeight = 300;
     x = -width()/2 + launchPadOffSet;
     y = 5;
     isconnected = false;
-    serverHeight =0.0;
+    serverHeight = 0.0;
     serverWidth = 0.0;
+    real_width = 0.0;
+    real_height = 0.0;
     in_air = false;
     this->setMinimumSize(100, 100);
-    //this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
-    this->resize(200, 290);
+    this->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+    lastHeight = 0.0;
+    lastWidth = 0.0;
     update();
 
 }
 
 void RocketSceneFinal::paintEvent(QPaintEvent *evnt){
+    if (serverWidth > 0 && serverHeight > 0) {
+    aspectratio = static_cast<double>(serverHeight) / serverWidth;
+    double widgetAspectRatio = static_cast<double>(height()) / width();
+
+    if (widgetAspectRatio > aspectratio) {
+        
+        real_width = width();
+        real_height = real_width * aspectratio;
+    } else {
+        
+        real_height = height();
+        real_width = real_height / aspectratio;
+    }
+}else {
+    real_width = width();
+    real_height = height();
+}
+
+    double scaleX = real_width/300;
+    double scaleY = real_height/300;
+
+
+    int sceneWidth = (serverWidth > 0) ? serverWidth : defaultWidth;
+    int sceneHeight = (serverHeight > 0) ? serverHeight : defaultHeight;
+    if (serverWidth != lastWidth || serverHeight != lastHeight) {
+    lastWidth = serverWidth;
+    lastHeight = serverHeight;
+    }
+
     if (serverHeight !=0.0 & serverWidth !=0.0){
-        Hscale = static_cast<double>(height())/serverHeight;
-        Wscale = static_cast<double>(width())/serverWidth;
+        Hscale = real_height/serverHeight;
+        Wscale = real_width/serverWidth;
     }else{
         Wscale = 1.0;
         Hscale = 1.0;
@@ -31,18 +64,29 @@ void RocketSceneFinal::paintEvent(QPaintEvent *evnt){
     QPainter painter(this);
     painter.translate(width()/2, height());
     painter.scale(1, -1);
+    painter.setBrush(Qt::green);
+    if (width() > real_width) {
+        int sideMargin = (width() - real_width) / 2;
+        painter.drawRect(-width()/2, 0, sideMargin, height());  // слева
+        painter.drawRect(real_width/2, 0, sideMargin, height()); // справа
+    }
 
+// Верхняя и нижняя горизонтальные полосы
+    if (height() > real_height) {
+        int topBottomMargin = (height() - real_height);
+        painter.drawRect(-width()/2, real_height, width(), topBottomMargin );
+    }
     painter.setBrush(Qt::black);
-    painter.drawRect(-width()/2, 0, width(), height());
+    painter.drawRect(-real_width/2, 0, real_width, real_height);
 
     painter.setBrush(Qt::blue);
-    painter.drawRect(-width()/2 + launchPadOffSet*Wscale, 10, 50, 10);
-    painter.drawRect(width()/2 - launchPadOffSet*Wscale - 50, 10, 50, 10);
+    painter.drawRect(-real_width/2 + launchPadOffSet*Wscale, 10, 50*scaleX, 10*scaleY);
+    painter.drawRect(real_width/2 - launchPadOffSet*Wscale - 50*scaleX, 10, 50*scaleX, 10*scaleY);
 
     painter.save();
 
     if(in_platform & serverHeight == 0.0 & serverWidth == 0.0){
-        x = -width()/2 + launchPadOffSet;
+        x = -real_width/2 + launchPadOffSet;
         qDebug()<< "disconnect";
 
     }else{
@@ -52,12 +96,12 @@ void RocketSceneFinal::paintEvent(QPaintEvent *evnt){
             Hscale = 1.0;
         }
     }
-    qDebug()<<"X"<<x;
-    painter.translate(x*Wscale + 25, y*Hscale);
-    painter.translate(0, 42);
+    qDebug()<<"Wscale: "<< Wscale;
+    painter.translate(x*Wscale + 25*scaleX, y*Hscale + 5*Hscale);
+    painter.translate(0, 42*scaleY);
     painter.rotate(rotation);
-    painter.translate(0, -22);
-    paintRocket(&painter);
+    painter.translate(0, -22*scaleY);
+    paintRocket(&painter, scaleX, scaleY);
 
     painter.restore();
 
@@ -65,42 +109,76 @@ void RocketSceneFinal::paintEvent(QPaintEvent *evnt){
 
 }
 
-void RocketSceneFinal::paintRocket(QPainter *painter) {
+void RocketSceneFinal::paintRocket(QPainter *painter, double scaleX, double scaleY) {
 
     // Рисуем основное тело
     painter->scale(1, -1);
     painter->setBrush(Qt::red);
-    painter->drawRect(-12, -48, 24, 48);
+    double bodyW = 12 * scaleX;
+    double bodyH = 48 * scaleY;
+    painter->drawRect(-bodyW, -bodyH, 2 * bodyW, bodyH);
+    //painter->drawRect(-12, -48, 24, 48);
 
     // Рисуем нос ракеты
     QPolygonF nose;
-    nose << QPointF(-12, -48) << QPointF(12, -48) << QPointF(0, -68);
+    nose << QPointF(-bodyW, -bodyH) << QPointF(bodyW, -bodyH) << QPointF(0, -bodyH - 20 * scaleY);
     painter->setBrush(Qt::gray);
     painter->drawPolygon(nose);
 
     // Рисуем двигатели
+
+
+
+    double engineW = 6 * scaleX;
+    double engineH = 6 * scaleY;
     painter->setBrush(Qt::yellow);
-    painter->drawRect(-10, 0, 6, 6);
-    painter->drawRect(4, 0, 6, 6);
+    painter->drawRect(-10 * scaleX, 0, engineW, engineH);
+    painter->drawRect(4 * scaleX, 0, engineW, engineH);
+
+    //painter->setBrush(Qt::yellow);
+    //painter->drawRect(-10, 0, 6, 6);
+    //painter->drawRect(4, 0, 6, 6);
 
     // Дополнительные элементы
-    painter->setBrush(Qt::darkGray);
-    painter->drawEllipse(QPointF(0, 5), 4, 6);
 
-    if (in_air) {
+    //painter->setBrush(Qt::darkGray);
+    //painter->drawEllipse(QPointF(0, 5), 4, 6);
+
+    //if (in_air) {
         // Цвет огня
-        painter->setBrush(QColor(255, 165, 0));
+    //    painter->setBrush(QColor(255, 165, 0));
+    //    QPolygonF flame;
+    //    flame << QPointF(-10, 5) 
+    //          << QPointF(10, 5)   
+    //          << QPointF(0, 20); 
+    //    painter->drawPolygon(flame);
+
+    //    painter->setBrush(Qt::yellow);
+    //    QPolygonF innerFlame;
+    //    innerFlame << QPointF(-6, 6)
+    //               << QPointF(6, 6)   
+    //               << QPointF(0, 15); 
+    //    painter->drawPolygon(innerFlame);
+    //}
+
+
+    painter->setBrush(Qt::darkGray);
+    painter->drawEllipse(QPointF(0, 5 * scaleX), 4 * scaleX, 6 * scaleY);
+
+    // Огонь если в воздухе
+    if (in_air) {
+        painter->setBrush(QColor(255, 165, 0)); // оранжевый
         QPolygonF flame;
-        flame << QPointF(-10, 5) 
-              << QPointF(10, 5)   
-              << QPointF(0, 20); 
+        flame << QPointF(-10 * scaleX, 5 * scaleY)
+              << QPointF(10 * scaleX, 5 * scaleY)
+              << QPointF(0, 20 * scaleY);
         painter->drawPolygon(flame);
 
         painter->setBrush(Qt::yellow);
         QPolygonF innerFlame;
-        innerFlame << QPointF(-6, 6)
-                   << QPointF(6, 6)   
-                   << QPointF(0, 15); 
+        innerFlame << QPointF(-6 * scaleX, 6 * scaleY)
+                   << QPointF(6 * scaleX, 6 * scaleY)
+                   << QPointF(0, 15 * scaleY);
         painter->drawPolygon(innerFlame);
     }
 }
@@ -114,13 +192,9 @@ void RocketSceneFinal::setPositionUpdate(int x, int y){
 }
 
 
+
+
+
 QSize RocketSceneFinal::sizeHint() const {
-    
     return QSize(serverWidth, serverHeight); 
-}
-
-
-void RocketSceneFinal::resizeEvent(QResizeEvent *event){
-    updateGeometry();
-    update();
 }
